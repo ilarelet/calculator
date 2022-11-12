1a8db09cb3cb3db4f9c1957ece54209311015f38
Ilya Eletskiy <ilarelet@gmail.com>
2022-11-13 01:20:10 +0300

    variables assignment function added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//initializing calculation variables
 let number1 = 0;
 let number2 = 0;
 let operator = "";
 
+//Basic mathematical operator functions
 const calcAdd = (a,b) => a+b;
 const calcSubstract = (a,b) => a-b;
 const calcMultiply = (a,b) => a*b;
@@ -14,6 +16,7 @@ const calcDivide = (a,b) => {
     }
 }
 
+//buttons responsive design
 const buttons_bar=document.querySelector("#buttons-bar");
 const buttons = buttons_bar.querySelectorAll('button');
 buttons.forEach(element => {
@@ -21,6 +24,7 @@ buttons.forEach(element => {
     element.addEventListener('mouseout', e => e.target.style.backgroundColor='rgb(45, 45, 45)');
 });
 
+//function performing the actual calculation
 let operate = (a,b, op) => {
     if (op === "plus") {
         return calcAdd(a,b);
@@ -36,7 +40,23 @@ let operate = (a,b, op) => {
     };
 };
 
+//function, responsible for reading the mathmatical operator chosen
 const operButton = document.querySelectorAll(".operButton");
 operButton.forEach(element => {
     element.addEventListener('click', e => op = e.target.id);
+});
+
+//function, responsible for reading the variables to be used in the calculation
+const numButton = document.querySelectorAll(".numButton");
+operButton.forEach(element => {
+    element.addEventListener('click', e => {
+        if (op === ''){
+            a= e.target.id; //A is the first variable, hence if the operator is not yet picked we assign the value to it
+            
+        }
+        else {
+            b = e.target.id;// if the operator has been chosen tha velue becomes assigned to B
+
+        }
+    })
 });

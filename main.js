90bc0b10e2b88e5b7aa3b737b7570447701cd0ed
Ilya Eletskiy <ilarelet@gmail.com>
2022-11-16 22:32:05 +0300

    Clear button added

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //initializing calculation variables
-let number1 = 0;
-let number2 = 0;
+let number1 = null;
+let number2 = null;
 let operator = "";
 
+
+//result text field initiatialization
+const resultText = document.querySelector('#result-text');
+resultText.textContent='0';
+
 //Basic mathematical operator functions
-const calcAdd = (a,b) => a+b;
-const calcSubstract = (a,b) => a-b;
-const calcMultiply = (a,b) => a*b;
-const calcDivide = (a,b) => {
+const add = (a,b) => a+b;
+const substract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => {
     if (b===0){
         alert("Do not devide by zero!");
     }
@@ -26,37 +31,49 @@ buttons.forEach(element => {
 
 //function performing the actual calculation
 let operate = (a,b, op) => {
-    if (op === "plus") {
-        return calcAdd(a,b);
+    if (op === "+") {
+        return add(a,b);
     }
-    else if (op === "minus") {
-        return calcSubstract(a,b);
+    else if (op === "-") {
+        return substract(a,b);
     }
-    else if (op === "multiply") {
-        return calcMultiply(a,b);
+    else if (op === "*") {
+        return multiply(a,b);
     }
-    else if (op === "divide") {
-        return calcDivide (a,b);
+    else if (op === "/") {
+        return divide (a,b);
     };
 };
 
 //function, responsible for reading the mathmatical operator chosen
-const operButton = document.querySelectorAll(".operButton");
+const operButton = document.querySelectorAll(".oper-button");
 operButton.forEach(element => {
-    element.addEventListener('click', e => op = e.target.id);
+    element.addEventListener('click', e => {
+        operator = e.target.id;
+        resultText.textContent+=operator;
+    });
 });
 
 //function, responsible for reading the variables to be used in the calculation
-const numButton = document.querySelectorAll(".numButton");
-operButton.forEach(element => {
-    element.addEventListener('click', e => {
-        if (op === ''){
-            a= e.target.id; //A is the first variable, hence if the operator is not yet picked we assign the value to it
-            
+const numButton = document.querySelectorAll(".num-button");
+numButton.forEach(element => {
+    element.addEventListener('click', (e,operator) => {
+        if ((operator === '') || (operator === undefined)){
+            number1 = e.target.id; //A is the first variable, hence if the operator is not yet picked we assign the value to it
+            resultText.textContent='number1';
         }
         else {
-            b = e.target.id;// if the operator has been chosen tha velue becomes assigned to B
-
+            number2 = e.target.id;// if the operator has been chosen tha velue becomes assigned to B
+            resultText.textContent+='number2';
         }
     })
 });
+
+//clear button
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', function(){
+    let number1 = null;
+    let number2 = null;
+    let operator = "";
+    resultText.textContent = "0";
+})
